cc50d818a8240442d3d4bb0b1eedc20316955bf3
random <random.chen@outlook.com>
2022-09-30 20:22:01 +0800

    thrid commit

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("asdf");
         System.out.println("dsf");
+        System.out.println("thi");
     }
 }
