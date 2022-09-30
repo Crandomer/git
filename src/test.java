6c1c48d00342144de93cac14eb8dbcf6b6b61d60
random <random.chen@outlook.com>
2022-09-30 20:29:59 +0800

    hot commit2

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
         System.out.println("dsf");
         System.out.println("thi");
         System.out.println("hot");
+        System.out.println("hot fix commit");
     }
 }
